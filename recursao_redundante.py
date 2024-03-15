24e941223c88b15c700cb753cba905ca0470fee2
AbnerCerqueira <abner.cerqueira@fatec.sp.gov.br>
2024-03-14 21:31:46 -0300

    recursão redundante exemplos

---
+++ recursao_redundante.py
@@ -0,0 +1,20 @@
+# recursao redundante ocorre quando a decomposição recursiva do problema produz varios subproblemas repetidamente
+# fatorial
+def fat(n):
+    if n == 0: return 1
+    else: return n * fat(n- 1)
+
+# potencia
+def pot(base, expoente):
+    if expoente == 0: return 1
+    else: return base * pot(base, expoente - 1)
+    
+# termial
+def term(n):
+    if n == 0: return 0
+    else: return n + term(n - 1) 
+
+# sequencia de fibonacci
+def fibo(n):
+    if n < 3: return 1
+    else: return fibo(n - 1) + fibo(n - 2)
